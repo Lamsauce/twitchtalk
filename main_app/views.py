f490744356e4759a049ad11367d9384c22913515
Lamsauce <lilylam743@gmail.com>
2021-02-04 15:44:50 -0800

    Added views and urls, templates currently being made. Making base html.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Home view
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    return HttpResponse('Register')
+
+def login(request):
+    return HttpResponse('Login')
+
+def games(request):
+    return HttpResponse('Games List')
