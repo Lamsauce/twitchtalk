7ff823b6ffe7c6807eeb55b34a2cf991efdacc55
Lamsauce <lilylam743@gmail.com>
2021-02-05 15:49:41 -0800

    registration redirects are now correct AND added the functionality to sign up, still needs TEST

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('accounts/register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('games/', views.games, name='games'),
     path('profile/', views.profile, name='profile'),
--- main_app/views.py
+++ main_app/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Thread, Comment
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 # Home view
 def home(request):
     return render(request, 'home.html', context = {"threads":Thread.objects.all})
 
 def register(request):
-    return render(request, 'register.html')
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid username or password. Please try again.'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/register.html', context)
 
 def login(request):
     return render(request, 'login.html')
