8fe060a556d5931287735c5f8a8426c551eec715
Lamsauce <lilylam743@gmail.com>
2021-02-05 11:54:49 -0800

    Threads not appearing on webpage, but admin creation works

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Thread(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(max_length=3000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Comment(models.Model):
+    description = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
