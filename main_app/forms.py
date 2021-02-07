50dd558bb8079756b65ab8f5a237259d55ec2c23
Lamsauce <lilylam743@gmail.com>
2021-02-07 11:20:15 -0800

    Customized registraion form to add email, no email auth yet and not redirecting properly

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,28 @@
 """ from django import forms
+from django.contrib.auth import login, authenticate, get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from .models import Thread, Comment
 
-class ThreadForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+    twitch_url = forms.URLField(label='Your Twitch URL', required=False, widget=forms.URLInput(
+        attrs={'class': 'form-control'}))
+
     class Meta:
-        model = Thread
-        fields = ['title', 'description'] """
+        fields = ['username', 'email', 'twitch_url', 'password1', 'password2'] """
+
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(ModelForm):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput)
+    twitch_url = forms.URLField(max_length=100, label='Twitch URL')
+    
+    class Meta:
+        model = User
+        fields = ["username", "password", "email", "twitch_url"]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Thread, Comment
 from django.http import HttpResponseRedirect
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .forms import RegisterForm
+
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -13,7 +18,7 @@ def home(request):
 def register(request):
     error_message = ''
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        add_form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
@@ -21,7 +26,7 @@ def register(request):
         else:
             error_message = 'Invalid username or password. Please try again.'
 
-    form = UserCreationForm()
+    form = RegisterForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/register.html', context)
 
